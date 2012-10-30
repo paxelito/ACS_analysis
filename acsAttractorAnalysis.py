abacce7d7c5f8cc56aae464ba5d82621595bf228
Alessandro Filisetti <alessandro.filisetti@gmail.com>
2012-10-30 17:38:24 +0100

    Version 1 is ready
    Version 1.0 is ready, a strange problem concerning the numpy.arccos
    function has to be fixed.

--- acsAttractorAnalysis.py
+++ acsAttractorAnalysis.py
@@ -7,22 +7,13 @@ import sys, os # Standard library
 import datetime as dt
 import linecache as lc
 import glob
+import numpy as np # Scientific library
+from numpy import * 
 
 try:
     from pylab import *
 except:
     pass
-
-def orderDictionary(tmpDict):
-	newList = {}
-	for key in sorted(tmpDict.iterkeys()):
-		newList[key] = tmpDict[key]
-	return newList
-	
-def orderDictionary2(adict): 
-	keys = adict.keys() 
-	keys.sort() 
-	return [adict[key] for key in keys]
 	
 # get path for placing simulation
 try:
@@ -35,12 +26,15 @@ print "Simulation Results Path: ", StrPath
 today = dt.date.today()
 	
 tmpDirs = os.listdir(StrPath)
-
-print tmpDirs
-	
+allSortedSpecies = [] 
+allConcentrations = [] 
+print ''
+print 'ACS ATTRACTORS ANALYSER'
+print ''
+print '|- STEP 1. Creating common sorted species list...'
 for tmpDir in tmpDirs:
 	os.chdir(StrPath)
-	totDirName = StrPath + '/' + tmpDir
+	totDirName = os.path.join(StrPath,tmpDir)
 	if os.path.isdir(totDirName):
 		# Move to the directory 
 		os.chdir(totDirName)
@@ -52,13 +46,12 @@ for tmpDir in tmpDirs:
 			speciesFiles = sorted(glob.glob(os.path.join(resDirPath,'species_*')))
 			speciesFile = speciesFiles[-1]
 		
-			print " "
-			print "|- Species File: ", speciesFile
+			print " |- Species File: ", speciesFile
 			# Open Catalysis File
 			try:
 				fidSpecies = open(speciesFile, 'r')
 			except:
-				print 'impossible to load ', speciesFile; sys.exit(1)
+				print ' |- impossible to load ', speciesFile; sys.exit(1)
 			
 			# For each last species file
 			seq = []; conc = []; speciesConc = []
@@ -67,14 +60,100 @@ for tmpDir in tmpDirs:
 				if (int(tmpCpxCut) == 0) & (float(tmpConc) > 0):
 					seq.append(str(tmpSeq))
 					conc.append(float(tmpConc))
-					speciesConc.append([str(tmpSeq),float(tmpConc)])
-					
-			#print speciesConc			
-			print "-------------"
+					speciesConc.append([str(tmpSeq),float(tmpConc)])	
+			# List sorting 		
 			speciesConc.sort()
-			print " "
-			print speciesConc
-			print " "
+			# Common ordered species List 
+			allSortedSpecies = list(set(seq) | set(allSortedSpecies))
+			allSortedSpecies.sort()	
+			
+			fidSpecies.close()	
+			
+print '|- STEP 2. Creating species concentration lists according to the overall sorted species list...'		
+overallConcList = []
+numberOfFolders = 0
+for tmpDir in tmpDirs:
+	os.chdir(StrPath)
+	totDirName = os.path.join(StrPath,tmpDir)
+	if os.path.isdir(totDirName):
+		# Move to the directory 
+		os.chdir(totDirName)
+		resDirPath = os.path.join(totDirName, "res")
+		if os.path.isdir(resDirPath):
+			numberOfFolders += 1
+			os.chdir(resDirPath)
+		
+			# Searching for files
+			speciesFiles = sorted(glob.glob(os.path.join(resDirPath,'species_*')))
+			speciesFile = speciesFiles[-1]
+		
+			print " |- Species File: ", speciesFile
+			# Open Catalysis File
+			try:
+				fidSpecies = open(speciesFile, 'r')
+			except:
+				print ' |- impossible to load ', speciesFile; sys.exit(1)
+			
+			# For each last species file
+			seq = []; conc = []; speciesConc = []
+			for sp in fidSpecies:
+				tmpID, tmpSeq, tmpConc, tmpDiff, tmpSol, tmpCpxDiss, tmpCpxCut, tmpEval, tmpAge, tmpReb, tmpCatID, tmpSubID, tmpKpho, tmpLoadConc, tmpConcLock = sp.split()
+				if (int(tmpCpxCut) == 0) & (float(tmpConc) > 0):
+					seq.append(str(tmpSeq))
+					conc.append(float(tmpConc))
+					
+			# Check the presence of the species in the all common species list		
+			for key in allSortedSpecies:
+				try:
+					pos = seq.index(key)
+					speciesConc.append(conc[pos])
+				except:
+					speciesConc.append(0)
+			
+			# Add the concentration list in the concentrationS list
+			overallConcList.append(speciesConc)		
+			
+			fidSpecies.close()	
+			
+print '|- STEP 3. Compute attractors differences (in term of different multi-dimensional angles)'	
+overallResMatrix = np.zeros((numberOfFolders,numberOfFolders))
+overallResMatrix_angle = np.zeros((numberOfFolders,numberOfFolders))
+# Angle between lists is now computed
+for idx, lx in enumerate(overallConcList):
+	for idy, ly in enumerate(overallConcList):
+		vecX = np.array(lx)
+		vecY = np.array(ly)
+		# Compute coseno
+		tmpCos = np.dot(vecX,vecY) / (np.linalg.norm(vecX) * np.linalg.norm(vecY))
+		overallResMatrix[idx,idy] = tmpCos
+		overallResMatrix_angle[idx,idy] = np.arccos(tmpCos)
+
+print '|- STEP 3. Save Files'
+os.chdir(StrPath)
+# '''Function to save statistic on file'''
+outFnameStat = 'acsAttractorsAnalysis.csv'
+saveFileStat = open(outFnameStat, 'w')
+cnt = 0
+for i in range(numberOfFolders):
+	strTypes = ''
+	for j in range(numberOfFolders):
+		strTypes += str(overallResMatrix[j,i]) + '\t'	
+	strTypes += '\n'
+	saveFileStat.write(strTypes)
+	cnt += 1
+saveFileStat.close()	
+
+
+
+print '|- FINISHED... SEE YOU NEXT TIME'		
+			
+
+			
+					
+						
+				
+			
+			
 
 			
     			
@@ -82,7 +161,7 @@ for tmpDir in tmpDirs:
 
 					
 					
-			fidSpecies.close()	
+
 			
 				
 
