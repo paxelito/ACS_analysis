b76ed9e1cc80b9b021e8a23ab468f3b2b0e6206c
Alessandro Filisetti <alessandro.filisetti@gmail.com>
2013-09-27 11:35:15 +0200

    k

--- acsStatesAnalysis.py
+++ acsStatesAnalysis.py
@@ -408,9 +408,6 @@ for IDdir, tmpDir in enumerate(tmpDirs):
 				  speciesConcs = speciesConcs[:,speciesConcs.sum(0)!=0]
 				  # Save concentrations matrix
 				  filename = "STAT_species_Concentrations_"  + str(validDir) + "_" + str(ngen) + ".csv"
-				  print ndn
-				  print StrPath
-				  print filename
 				  print os.path.join(StrPath,ndn,filename)
 				  np.savetxt(os.path.join(StrPath,ndn,filename), speciesConcs, delimiter='\t', fmt='%.5f')
 			validDir += 1
