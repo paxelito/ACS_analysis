c7cd8b1961df557f86c4c6aa9a7b9ebbdac36284
Alessandro Filisetti <alessandro.filisetti@gmail.com>
2013-09-30 14:18:42 +0200

    j

--- acsAttractorAnalysis.py
+++ acsAttractorAnalysis.py
@@ -200,7 +200,7 @@ for idx, lx in enumerate(overallConcListNOINFLUX):
 		vecX = np.array(lx)
 		vecY = np.array(ly)
 		# Compute coseno
-		tmpCos = float(np.dot(vecX,vecY) / (np.linalg.norm(vecX) * np.linalg.norm(vecY)))
+		tmpCos = float(np.dot(vecX,vecY) / (np.linalg.norm(vecX) * np.linalg.norm(vecY))) + 0
 		ANG_overallResMatrixNOINFLUX[idx,idy] = np.arccos(tmpCos)  * 360.00 / 6.28
 		#ANG_overallResMatrixNOINFLUX[idx,idy] = tmpCos
 		print tmpCos, " ", np.arccos(tmpCos)," ",ANG_overallResMatrixNOINFLUX[idx,idy] 
